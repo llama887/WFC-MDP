feafc5bafcf44fcfc2d4c3937ae347e0885e2325
Franklin Yiu <67379586+llama887@users.noreply.github.com>
2025-05-30 18:36:20 -0400

    Franklin/2d (#11)
    fixes to plotting to make it work better with the 2d representation

--- core/wfc_env.py
+++ core/wfc_env.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Add the project root to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import random
 from typing import Any, Callable
 
