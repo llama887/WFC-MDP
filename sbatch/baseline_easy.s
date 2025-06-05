10811c5278f5e1ca11029e6172b23a6a4855ca88
Franklin Yiu <mail.fyiu@gmail.com>
2025-06-04 23:03:30 -0400

    including sbatchs

---
+++ sbatch/baseline_easy.s
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=baseline_plots_easy
+#SBATCH --output=baseline_plots_easy%j.out
+#SBATCH --error=baseline_plots_easy%j.err
+#SBATCH --mail-type=START,END,FAIL
+#SBATCH --mail-user=fyy2003@nyu.edu
+#SBATCH --time=10:00:00 
+#SBATCH --nodes=1                     
+#SBATCH --ntasks-per-node=1           
+#SBATCH --cpus-per-task=48           
+#SBATCH --mem=128G                             
+#SBATCH --account=pr_100_tandon_priority
+
+### -------------------- Logging Setup -------------------- ###
+LOG_DIR="/scratch/$USER/optimizing_WFC/output"
+GPU_LOG_FILE="$LOG_DIR/gpu_used_${SLURM_JOB_ID}.txt"
+mkdir -p $LOG_DIR
+log_and_email() {
+    MESSAGE="$1"
+    echo "$MESSAGE" | tee -a "$GPU_LOG_FILE"
+    echo -e "Subject:[Slurm Job: $SLURM_JOB_ID] Status Update\n\n$MESSAGE" | sendmail fyy2003@nyu.edu
+}
+log_and_email "Starting job: $SLURM_JOB_NAME ($SLURM_JOB_ID)"
+
+module purge
+cd /scratch/fyy2003/optimizing_WFC
+source venv/bin/activate
+python fi2pop.py   -l hyperparameters/binary_hyperparameters.yaml   --easy   --binary --bar-graph
+
