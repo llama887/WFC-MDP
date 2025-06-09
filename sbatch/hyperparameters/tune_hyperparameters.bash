18e6cdd18c0a80db261b61bb2d392f60e77cabcb
Franklin Yiu <mail.fyiu@gmail.com>
2025-06-09 01:38:53 -0400

    sbatch for fi2pop

---
+++ sbatch/hyperparameters/tune_hyperparameters.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sbatch sbatch/hyperparameters/binary_1d.s
+sbatch sbatch/hyperparameters/binary_2d.s
+sbatch sbatch/hyperparameters/combo_grass_1d.s
+sbatch sbatch/hyperparameters/combo_grass_2d.s
+sbatch sbatch/hyperparameters/combo_pond_1d.s
+sbatch sbatch/hyperparameters/combo_pond_2d.s
+sbatch sbatch/hyperparameters/combo_river_1d.s
+sbatch sbatch/hyperparameters/combo_river_2d.s
+
+sbatch sbatch/hyperparameters/binary_1d_fi2pop.s
+sbatch sbatch/hyperparameters/binary_2d_fi2pop.s
+sbatch sbatch/hyperparameters/combo_grass_1d_fi2pop.s
+sbatch sbatch/hyperparameters/combo_grass_2d_fi2pop.s
+sbatch sbatch/hyperparameters/combo_pond_1d_fi2pop.s
+sbatch sbatch/hyperparameters/combo_pond_2d_fi2pop.s
+sbatch sbatch/hyperparameters/combo_river_1d_fi2pop.s
+sbatch sbatch/hyperparameters/combo_river_2d_fi2pop.s
