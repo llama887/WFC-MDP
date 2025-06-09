049f9f98e0fa1093c807dc20d442ea3b0790337a
Franklin Yiu <mail.fyiu@gmail.com>
2025-06-09 01:44:16 -0400

    mcts hyperparameter tunign sbatch

---
+++ sbatch/hyperparameters/combo_grass_1d_mcts.s
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=hyperparams_combo_grass_1d_mcts
+#SBATCH --output=hyperparams_combo_grass_1d_mcts_%j.out
+#SBATCH --error=hyperparams_combo_grass_1d_mcts_%j.err
+#SBATCH --mail-type=START,END,FAIL
+#SBATCH --mail-user=fyy2003@nyu.edu
+#SBATCH --time=48:00:00 
+#SBATCH --nodes=1                     
+#SBATCH --ntasks-per-node=1           
+#SBATCH --cpus-per-task=48           
+#SBATCH --mem=128G                             
+#SBATCH --account=pr_100_tandon_priority
+
+### -------------------- Logging Setup -------------------- ###
+LOG_DIR="/scratch/$USER/optimizing_WFC/output"
+GPU_LOG_FILE="$LOG_DIR/gpu_used_${SLURM_JOB_ID}.txt"
+mkdir -p $LOG_DIR
+log_and_email() {
+    MESSAGE="$1"
+    echo "$MESSAGE" | tee -a "$GPU_LOG_FILE"
+    echo -e "Subject:[Slurm Job: $SLURM_JOB_ID] Status Update\n\n$MESSAGE" | sendmail fyy2003@nyu.edu
+}
+log_and_email "Starting job: $SLURM_JOB_NAME ($SLURM_JOB_ID)"
+
+module purge
+cd /scratch/fyy2003/optimizing_WFC
+source venv/bin/activate
+python core/mcts.py --generations-per-trial 100 --hyperparameter-dir hyperparameters --output-file mcts_combo_grass_1d_hyperparameters.yaml --task binary_hard --task grass --optuna-trials 20
+
+
+
--- sbatch/hyperparameters/tune_hyperparameters.bash
+++ sbatch/hyperparameters/tune_hyperparameters.bash
@@ -17,3 +17,12 @@ sbatch sbatch/hyperparameters/combo_pond_1d_fi2pop.s
 sbatch sbatch/hyperparameters/combo_pond_2d_fi2pop.s
 sbatch sbatch/hyperparameters/combo_river_1d_fi2pop.s
 sbatch sbatch/hyperparameters/combo_river_2d_fi2pop.s
+
+sbatch sbatch/hyperparameters/binary_1d_mcts.s
+sbatch sbatch/hyperparameters/binary_2d_mcts.s
+sbatch sbatch/hyperparameters/combo_grass_1d_mcts.s
+sbatch sbatch/hyperparameters/combo_grass_2d_mcts.s
+sbatch sbatch/hyperparameters/combo_pond_1d_mcts.s
+sbatch sbatch/hyperparameters/combo_pond_2d_mcts.s
+sbatch sbatch/hyperparameters/combo_river_1d_mcts.s
+sbatch sbatch/hyperparameters/combo_river_2d_mcts.s
