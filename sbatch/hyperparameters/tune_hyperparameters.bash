3c064eedd858dd405a23bad944b40d423df35fd4
Franklin Yiu <mail.fyiu@gmail.com>
2025-06-11 00:12:30 -0400

    fix path issues in hyperparameter bash script

--- sbatch/hyperparameters/tune_hyperparameters.bash
+++ sbatch/hyperparameters/tune_hyperparameters.bash
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-sbatch sbatch/hyperparameters/binary_1d.s
-sbatch sbatch/hyperparameters/binary_2d.s
-sbatch sbatch/hyperparameters/combo_grass_1d.s
-sbatch sbatch/hyperparameters/combo_grass_2d.s
-sbatch sbatch/hyperparameters/combo_pond_1d.s
-sbatch sbatch/hyperparameters/combo_pond_2d.s
-sbatch sbatch/hyperparameters/combo_river_1d.s
-sbatch sbatch/hyperparameters/combo_river_2d.s
+sbatch binary_1d.s
+sbatch binary_2d.s
+sbatch combo_grass_1d.s
+sbatch combo_grass_2d.s
+sbatch combo_pond_1d.s
+sbatch combo_pond_2d.s
+sbatch combo_river_1d.s
+sbatch combo_river_2d.s
 
-sbatch sbatch/hyperparameters/binary_fi2pop.s
-sbatch sbatch/hyperparameters/combo_grass_fi2pop.s
-sbatch sbatch/hyperparameters/combo_pond_fi2pop.s
-sbatch sbatch/hyperparameters/combo_river_fi2pop.s
+sbatch binary_fi2pop.s
+sbatch combo_grass_fi2pop.s
+sbatch combo_pond_fi2pop.s
+sbatch combo_river_fi2pop.s
 
-sbatch sbatch/hyperparameters/binary_baseline.s
-sbatch sbatch/hyperparameters/combo_grass_baseline.s
-sbatch sbatch/hyperparameters/combo_pond_baseline.s
-sbatch sbatch/hyperparameters/combo_river_baseline.s
+sbatch binary_baseline.s
+sbatch combo_grass_baseline.s
+sbatch combo_pond_baseline.s
+sbatch combo_river_baseline.s
 
-sbatch sbatch/hyperparameters/binary_mcts.s
-sbatch sbatch/hyperparameters/combo_grass_mcts.s
-sbatch sbatch/hyperparameters/combo_pond_mcts.s
-sbatch sbatch/hyperparameters/combo_river_mcts.s
+sbatch binary_mcts.s
+sbatch combo_grass_mcts.s
+sbatch combo_pond_mcts.s
+sbatch combo_river_mcts.s
