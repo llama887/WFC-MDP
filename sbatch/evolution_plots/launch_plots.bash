38f46b16f83d2d2492657864e3c2690a546cb33a
Franklin Yiu <mail.fyiu@gmail.com>
2025-06-16 00:12:52 -0400

    sbatches for 0 random offspring ablation

--- sbatch/evolution_plots/launch_plots.bash
+++ sbatch/evolution_plots/launch_plots.bash
@@ -18,3 +18,23 @@ sbatch sbatch/evolution_plots/plot_grass_hard.s
 sbatch sbatch/evolution_plots/plot_binary_hard.s 
 sbatch sbatch/evolution_plots/plot_binary_easy.s 
 sbatch sbatch/evolution_plots/plot_biomes.s 
+
+sbatch sbatch/evolution_plots/plot_binary_easy_2d_0.s 
+sbatch sbatch/evolution_plots/plot_binary_hard_2d_0.s 
+sbatch sbatch/evolution_plots/plot_grass_hard_2d_0.s 
+sbatch sbatch/evolution_plots/plot_grass_easy_2d_0.s 
+sbatch sbatch/evolution_plots/plot_pond_easy_2d_0.s 
+sbatch sbatch/evolution_plots/plot_pond_hard_2d_0.s 
+sbatch sbatch/evolution_plots/plot_river_hard_2d_0.s 
+sbatch sbatch/evolution_plots/plot_river_easy_2d_0.s 
+sbatch sbatch/evolution_plots/plot_biomes_2d_0.s 
+sbatch sbatch/evolution_plots/plot_river_easy_0.s 
+sbatch sbatch/evolution_plots/plot_river_hard_0.s 
+sbatch sbatch/evolution_plots/plot_pond_hard_0.s 
+sbatch sbatch/evolution_plots/plot_pond_easy_0.s 
+sbatch sbatch/evolution_plots/plot_grass_easy_0.s 
+sbatch sbatch/evolution_plots/plot_grass_hard_0.s 
+sbatch sbatch/evolution_plots/plot_binary_hard_0.s 
+sbatch sbatch/evolution_plots/plot_binary_easy_0.s 
+sbatch sbatch/evolution_plots/plot_biomes_0.s 
+
