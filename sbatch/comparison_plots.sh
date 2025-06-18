babbcffd0b632da0572c13d2afc54bf7c0f89074
Franklin Yiu <mail.fyiu@gmail.com>
2025-06-18 16:42:27 -0400

    0_random_offspring

--- sbatch/comparison_plots.sh
+++ sbatch/comparison_plots.sh
@@ -2,6 +2,7 @@
 
 mkdir comparison_figures
 echo "Plotting..."
+source venv/bin/activate
 python plot.py --compare --csv-files figures_baseline/binary_convergence.csv \
                                     figures_evolution/1d/binary_easy_convergence.csv \
                                     figures_evolution/2d/binary_easy_convergence.csv \
@@ -26,13 +27,13 @@ python plot.py --compare --csv-files figures_baseline/combo_grass_convergence.cs
                                     --title "Grass Combo Convergence Behavior" \
                                     --output comparison_figures/grass_comparison.png &
 
-python plot.py --compare --csv-files figures_baseline/combo_grass_hard_convergence.csv \
-                                    figures_evolution/1d/combo_grass_hard_convergence.csv \
-                                    figures_evolution/2d/combo_grass_hard_convergence.csv \
-                                    figures_fi2pop/combo_grass_hard_convergence.csv \
-                                    --labels baseline evolution_1d evolution_2d fi2pop \
-                                    --title "Hard Grass Combo Convergence Behavior" \
-                                    --output comparison_figures/grass_hard_comparison.png &
+# python plot.py --compare --csv-files figures_baseline/combo_grass_hard_convergence.csv \
+#                                     figures_evolution/1d/combo_grass_hard_convergence.csv \
+#                                     figures_evolution/2d/combo_grass_hard_convergence.csv \
+#                                     figures_fi2pop/combo_grass_hard_convergence.csv \
+#                                     --labels baseline evolution_1d evolution_2d fi2pop \
+#                                     --title "Hard Grass Combo Convergence Behavior" \
+#                                     --output comparison_figures/grass_hard_comparison.png &
 
 python plot.py --compare --csv-files figures_baseline/combo_river_convergence.csv \
                                     figures_evolution/1d/combo_river_convergence.csv \
@@ -50,13 +51,13 @@ python plot.py --compare --csv-files figures_baseline/combo_river_hard_convergen
                                     --title "River Hard Combo Convergence Behavior" \
                                     --output comparison_figures/river_hard_comparison.png & 
 
-# python plot.py --compare --csv-files figures_baseline/combo_pond_convergence.csv \
-#                                     figures_evolution/1d/combo_pond_convergence.csv \
-#                                     figures_evolution/2d/combo_pond_convergence.csv \
-#                                     figures_fi2pop/combo_pond_convergence.csv \
-#                                     --labels baseline evolution_1d evolution_2d fi2pop \
-#                                     --title "Pond Combo Convergence Behavior" \
-#                                     --output comparison_figures/pond_comparison.png &
+python plot.py --compare --csv-files figures_baseline/combo_pond_convergence.csv \
+                                    figures_evolution/1d/combo_pond_convergence.csv \
+                                    figures_evolution/2d/combo_pond_convergence.csv \
+                                    figures_fi2pop/combo_pond_convergence.csv \
+                                    --labels baseline evolution_1d evolution_2d fi2pop \
+                                    --title "Pond Combo Convergence Behavior" \
+                                    --output comparison_figures/pond_comparison.png &
 
 python plot.py --compare --csv-files figures_baseline/combo_pond_hard_convergence.csv \
                                     figures_evolution/1d/combo_pond_hard_convergence.csv \
@@ -65,5 +66,72 @@ python plot.py --compare --csv-files figures_baseline/combo_pond_hard_convergenc
                                     --labels baseline evolution_1d evolution_2d fi2pop \
                                     --title "Pond Hard Combo Convergence Behavior" \
                                     --output comparison_figures/pond_hard_comparison.png &
+
+wait
+echo "Plotting ablations..."
+
+python plot.py --compare --csv-files figures_baseline/binary_convergence.csv \
+                                    figures_evolution/1d/0_random_offspring/binary_easy_convergence.csv \
+                                    figures_evolution/2d/0_random_offspring/binary_easy_convergence.csv \
+                                    figures_fi2pop/binary_convergence.csv \
+                                    --labels baseline evolution_1d evolution_2d fi2pop \
+                                    --title "Binary Easy Convergence Behavior" \
+                                    --output comparison_figures/0_random_offspring_binary_easy_comparison.png &
+
+python plot.py --compare --csv-files figures_baseline/binary_hard_convergence.csv \
+                                    figures_evolution/1d/0_random_offspring/binary_hard_convergence.csv \
+                                    figures_evolution/2d/0_random_offspring/binary_hard_convergence.csv \
+                                    figures_fi2pop/binary_hard_convergence.csv \
+                                    --labels baseline evolution_1d evolution_2d fi2pop \
+                                    --title "Binary Hard Convergence Behavior" \
+                                    --output comparison_figures/0_random_offspring_binary_hard_comparison.png &
+
+python plot.py --compare --csv-files figures_baseline/combo_grass_convergence.csv \
+                                    figures_evolution/1d/0_random_offspring/combo_grass_convergence.csv \
+                                    figures_evolution/2d/0_random_offspring/combo_grass_convergence.csv \
+                                    figures_fi2pop/combo_grass_convergence.csv \
+                                    --labels baseline evolution_1d evolution_2d fi2pop \
+                                    --title "Grass Combo Convergence Behavior" \
+                                    --output comparison_figures/0_random_offspring_grass_comparison.png &
+
+python plot.py --compare --csv-files figures_baseline/combo_grass_hard_convergence.csv \
+                                    figures_evolution/1d/0_random_offspring/combo_grass_hard_convergence.csv \
+                                    figures_evolution/2d/0_random_offspring/combo_grass_hard_convergence.csv \
+                                    figures_fi2pop/combo_grass_hard_convergence.csv \
+                                    --labels baseline evolution_1d evolution_2d fi2pop \
+                                    --title "Hard Grass Combo Convergence Behavior" \
+                                    --output comparison_figures/0_random_offspring_grass_hard_comparison.png &
+
+python plot.py --compare --csv-files figures_baseline/combo_river_convergence.csv \
+                                    figures_evolution/1d/0_random_offspring/combo_river_convergence.csv \
+                                    figures_evolution/2d/0_random_offspring/combo_river_convergence.csv \
+                                    figures_fi2pop/combo_river_convergence.csv \
+                                    --labels baseline evolution_1d evolution_2d fi2pop \
+                                    --title "River Combo Convergence Behavior" \
+                                    --output comparison_figures/0_random_offspring_river_comparison.png &
+
+python plot.py --compare --csv-files figures_baseline/combo_river_hard_convergence.csv \
+                                    figures_evolution/1d/0_random_offspring/combo_river_hard_convergence.csv \
+                                    figures_evolution/2d/0_random_offspring/combo_river_hard_convergence.csv \
+                                    figures_fi2pop/combo_river_hard_convergence.csv \
+                                    --labels baseline evolution_1d evolution_2d fi2pop \
+                                    --title "River Hard Combo Convergence Behavior" \
+                                    --output comparison_figures/0_random_offspring_river_hard_comparison.png & 
+
+python plot.py --compare --csv-files figures_baseline/combo_pond_convergence.csv \
+                                    figures_evolution/1d/0_random_offspring/combo_pond_convergence.csv \
+                                    figures_evolution/2d/0_random_offspring/combo_pond_convergence.csv \
+                                    figures_fi2pop/combo_pond_convergence.csv \
+                                    --labels baseline evolution_1d evolution_2d fi2pop \
+                                    --title "Pond Combo Convergence Behavior" \
+                                    --output comparison_figures/0_random_offspring_pond_comparison.png &
+
+python plot.py --compare --csv-files figures_baseline/combo_pond_hard_convergence.csv \
+                                    figures_evolution/1d/0_random_offspring/combo_pond_hard_convergence.csv \
+                                    figures_evolution/2d/0_random_offspring/combo_pond_hard_convergence.csv \
+                                    figures_fi2pop/combo_pond_hard_convergence.csv \
+                                    --labels baseline evolution_1d evolution_2d fi2pop \
+                                    --title "Pond Hard Combo Convergence Behavior" \
+                                    --output comparison_figures/0_random_offspring_pond_hard_comparison.png &
 
 wait
