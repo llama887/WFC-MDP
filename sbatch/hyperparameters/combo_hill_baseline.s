f17ff270e8b0251bff6847e5282661c505880c39
Franklin Yiu <mail.fyiu@gmail.com>
2025-08-24 06:36:07 -0400

    hill hyperparameter tuning scripts

---
+++ sbatch/hyperparameters/combo_hill_baseline.s
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=hyperparams_combo_hill_baseline
+#SBATCH --output=hyperparams_combo_hill_baseline_%j.out
+#SBATCH --error=hyperparams_combo_hill_baseline_%j.err
+#SBATCH --mail-type=START,END,FAIL
+#SBATCH --mail-user=fyy2003@nyu.edu
+#SBATCH --time=5:00:00
+#SBATCH --nodes=1                     
+#SBATCH --ntasks-per-node=1           
+#SBATCH --cpus-per-task=48           
+#SBATCH --mem=64G                             
+#SBATCH --account=pr_100_tandon_priority
+
+### -------------------- Logging Setup -------------------- ###
+LOG_DIR="/scratch/$USER/optimizing_WFC/output"
+GPU_LOG_FILE="$LOG_DIR/gpu_used_${SLURM_JOB_ID}.txt"
+mkdir -p $LOG_DIR
+log_and_email() {
+    MESSAGE="$1"
+    echo "$MESSAGE" | tee -a "$GPU_LOG_FILE"
+    echo -e "Subject:[Slurm Job: $SLURM_JOB_ID] Status Update\n\n$MESSAGE" | sendmail fyy2003@nyu.edu
+}
+log_and_email "Starting job: $SLURM_JOB_NAME ($SLURM_JOB_ID)"
+
+module purge
+cd /scratch/fyy2003/optimizing_WFC
+source venv/bin/activate
+python core/fi2pop.py --mode baseline --generations-per-trial 500 --hyperparameter-dir hyperparameters --output-file baseline_combo_hill_hyperparameters.yaml --task binary_hard --task hill --optuna-trials 5
+
+
+
--- sbatch/hyperparameters/tune_hyperparameters.bash
+++ sbatch/hyperparameters/tune_hyperparameters.bash
@@ -8,18 +8,17 @@ sbatch combo_pond_1d.s
 sbatch combo_pond_2d.s
 sbatch combo_river_1d.s
 sbatch combo_river_2d.s
+sbatch combo_hill_1d.s
+sbatch combo_hill_2d.s
 
 sbatch binary_fi2pop.s
 sbatch combo_grass_fi2pop.s
 sbatch combo_pond_fi2pop.s
 sbatch combo_river_fi2pop.s
+sbatch combo_hill_fi2pop.s
 
 sbatch binary_baseline.s
 sbatch combo_grass_baseline.s
 sbatch combo_pond_baseline.s
 sbatch combo_river_baseline.s
-
-sbatch binary_mcts.s
-sbatch combo_grass_mcts.s
-sbatch combo_pond_mcts.s
-sbatch combo_river_mcts.s
+sbatch combo_hill_baseline.s
